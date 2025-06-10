6842b3d3c0f06e7acbb8ade8d33088b3c32dac18
Caren <carenjepchumba4@gmail.com>
2025-06-10 11:58:12 +0300

    defined prompt

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,31 +1,23 @@
-const readline = require('readline');
+const prompt = require('prompt-sync')();
+// function to calculate boda fare
+function calculateBodaFare(distanceInKm){
+    distanceInKm = parseInt(prompt("Unafika wapi Mkubwa? Kilometer ngapi?:"));
 
-// Define the function
-function calculateBodaFare(distanceInKm) {
-// standard price
-  const baseFare = 50; // in KES
-  const chargePerKm = 15; // in KES
-// calculate total distance
-  const standardPrice = distanceInKm * chargePerKm;
-  const totalFare = baseFare + standardPrice;
+ // standard price
+const baseFare = 50; //in ksh
+const chargePerKm = 15; //in ksh
+
+//Calculate total distance
+const standardPrice = distanceInKm * chargePerKm;
+const totalFare = baseFare + (distanceInKm * chargePerKm);
 
 // output
-  console.log(`Uko kwote? Io ni ${distanceInKm} km:`);
-  console.log(`Ukikalia Pikipiki: KES ${baseFare}`);
-  console.log(`Mpaka Uko: KES ${standardPrice}`);
-  console.log(`Total: KES ${totalFare}`);
-  console.log("Panda Pikipiki!");
+console.log(`Uko kwote? Io ni ${distanceInKm} km:`);
+console.log(`Ukikalia Pikipiki: KES ${baseFare}`)
+console.log(`Mpaka Uko: KES ${standardPrice}`);
+console.log(`Total: KES ${totalFare}`);
+
+console.log("Panda Pikipiki!")
 }
-
-// Prompt the user and Call the function
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("Unafika wapi Mkubwa? Kilometer ngapi?: ", function (input) {
-  const distanceInKm = parseFloat(input);
-  calculateBodaFare(distanceInKm);
-
-  rl.close();
-});
+// calling function calculate
+calculateBodaFare();
