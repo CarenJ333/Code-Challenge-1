e32a958f14b9999c326b9a9d526b3f2962961474
Caren <carenjepchumba4@gmail.com>
2025-06-10 08:52:25 +0300

    Added more comments

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,21 +1,23 @@
 const readline = require('readline');
 
-// 1. Define the function
+// Define the function
 function calculateBodaFare(distanceInKm) {
+// standard price
   const baseFare = 50; // in KES
   const chargePerKm = 15; // in KES
-
+// calculate total distance
   const standardPrice = distanceInKm * chargePerKm;
   const totalFare = baseFare + standardPrice;
 
-  console.log(`\nUko kwote? Io ni ${distanceInKm} km:`);
+// output
+  console.log(`Uko kwote? Io ni ${distanceInKm} km:`);
   console.log(`Ukikalia Pikipiki: KES ${baseFare}`);
   console.log(`Mpaka Uko: KES ${standardPrice}`);
   console.log(`Total: KES ${totalFare}`);
   console.log("Panda Pikipiki!");
 }
 
-// 2. Prompt the user and 3. Call the function
+// Prompt the user and Call the function
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
